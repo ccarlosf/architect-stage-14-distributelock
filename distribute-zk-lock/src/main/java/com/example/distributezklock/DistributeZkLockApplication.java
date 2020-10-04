fd28bc9e1850611368a7ad15bd61cc1eb4f4ab24
ccarlos <2238513100@qq.com>
2020-10-04 15:45:49 +0800

    阶段三：分布式架构-逐个击破分布式核心问题（9-17周） 第14周   分布式锁 2-16 curator分布式锁

--- distribute-zk-lock/src/main/java/com/example/distributezklock/DistributeZkLockApplication.java
+++ distribute-zk-lock/src/main/java/com/example/distributezklock/DistributeZkLockApplication.java
@@ -1,5 +1,9 @@
 package com.example.distributezklock;
 
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,4 +15,10 @@ public class DistributeZkLockApplication {
         SpringApplication.run(DistributeZkLockApplication.class, args);
     }
 
+    @Bean(initMethod = "start", destroyMethod = "close")
+    public CuratorFramework getCuratorFramework() {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", retryPolicy);
+        return client;
+    }
 }
--- distribute-zk-lock/src/test/java/com/example/distributezklock/DistributeZkLockApplicationTests.java
+++ distribute-zk-lock/src/test/java/com/example/distributezklock/DistributeZkLockApplicationTests.java
@@ -2,6 +2,11 @@ package com.example.distributezklock;
 
 import com.example.distributezklock.lock.ZkLock;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,7 +29,28 @@ public class DistributeZkLockApplicationTests {
     public void testZkLock() throws Exception {
         ZkLock zkLock = new ZkLock();
         boolean lock = zkLock.getLock("order");
-        log.info("获得锁的结果："+lock);
+        log.info("获得锁的结果：" + lock);
+
         zkLock.close();
+    }
+
+    @Test
+    public void testCuratorLock() {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", retryPolicy);
+        client.start();
+        InterProcessMutex lock = new InterProcessMutex(client, "/order");
+        try {
+            if (lock.acquire(30, TimeUnit.SECONDS)) {
+                try {
+                    log.info("我获得了锁！！！");
+                } finally {
+                    lock.release();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        client.close();
     }
 }
